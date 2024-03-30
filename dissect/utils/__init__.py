82d6f84cff61e171fe45d77deefcef76c8c93eeb
Yawei Li <yawei.li.muc@gmail.com>
2024-03-30 13:02:32 +0800

    Implement hook to save the attention and mlp input and output.

--- dissect/utils/__init__.py
+++ dissect/utils/__init__.py
@@ -1,4 +1,5 @@
+from .input_key_strategies import get_input_key_mapping
 from .misc import calc_pruned_parameters, name_contains_keys
 from .typing import Device
 
-__all__ = ["Device", "name_contains_keys", "calc_pruned_parameters"]
+__all__ = ["Device", "name_contains_keys", "calc_pruned_parameters", "get_input_key_mapping"]
