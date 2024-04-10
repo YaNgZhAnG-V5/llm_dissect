7c1df2999d989bed54511c174049daedfde94d85
Yawei Li <yawei.li.muc@gmail.com>
2024-04-10 13:04:51 +0800

    Allows for saving all kwarg layer inputs.

--- dissect/utils/input_key_strategies.py
+++ dissect/utils/input_key_strategies.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Callable, List, Optional
 
 
 def get_input_key_mapping(strategy: str) -> Callable[[str], Optional[str]]:
@@ -10,9 +10,9 @@ def get_input_key_mapping(strategy: str) -> Callable[[str], Optional[str]]:
         raise NotImplementedError(f"Input mapping is not implemented for {strategy}")
 
 
-def vicuna_input_key_mapping(layer_name: str) -> Optional[str]:
+def vicuna_input_key_mapping(layer_name: str) -> Optional[List[str]]:
     if "self_attn" in layer_name:
-        return "hidden_states"
+        return ["hidden_states", "attention_mask", "position_ids"]
     elif "mlp" in layer_name:
         return None
     else:
