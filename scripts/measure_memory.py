d51021081aa71235df01821b94930b446e382736
Yang Zhang <zhangyang9523@gmail.com>
2024-04-15 02:07:44 +0000

    add memory and runtime measurement scripts

---
+++ scripts/measure_memory.py
@@ -0,0 +1,74 @@
+from argparse import ArgumentParser
+
+import mmengine
+import torch
+from alive_progress import alive_it
+
+from dissect.models import build_model_and_tokenizer
+
+
+def get_module_memory_consumption(module, device):
+    module.to(device)
+    memory_in_mb = torch.cuda.memory_allocated() / 1024 / 1024
+    module.to(torch.device("cpu"))
+    assert torch.cuda.memory_allocated(device=device) == 0.0
+    return memory_in_mb
+
+
+def get_model_memory_consumption_summary(model, device, verbose=False):
+    assert torch.cuda.memory_allocated(device=device) == 0.0, "GPU is not empty, cannot measure memory consumption."
+    memory_consumption = dict()
+    for name, module in alive_it(
+        model.named_modules(),
+        total=len(list(model.named_modules())),
+    ):
+        if name == "":
+            name = "overall"
+        module.to(torch.device("cuda:0"))
+        memory_in_mb = get_module_memory_consumption(module, device)
+        if verbose:
+            print(f"GPU Memory Requirement for {name}: {memory_in_mb} MiB")
+        memory_consumption.update({name: memory_in_mb})
+    return memory_consumption
+
+
+def parse_args():
+    parser = ArgumentParser("Test pruned models")
+    parser.add_argument("--config", default="./configs/prune_vicuna.yaml", help="Path to config file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="GPU ID.")
+    parser.add_argument(
+        "--cfg-options",
+        "-o",
+        nargs="+",
+        action=mmengine.DictAction,
+        help="Override the config entries with format xxx=yyy or xxx.zzz.qqq=yyy .",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    cfg = mmengine.Config.fromfile(args.config)
+    device = torch.device(f"cuda:{args.gpu_id}")
+    model, _ = build_model_and_tokenizer(cfg.model, device=device)
+    model.to(torch.device("cpu")).eval()
+    memory_consumption = get_model_memory_consumption_summary(model, device)
+
+    # get memory consumption summary for interested modules
+    modules_of_interest = ["self_attn", "mlp", "overall"]
+    summary_dict = dict()
+    for module_name in modules_of_interest:
+        summary_dict.update({module_name: 0.0})
+    for name, val in memory_consumption.items():
+        for module_name in modules_of_interest:
+            if name.split(".")[-1] == module_name:
+                print(f"GPU Memory Requirement for {name}: {val} MiB")
+                summary_dict[module_name] += val
+    print("Summary:")
+    for module_name, val in summary_dict.items():
+        print(f"Overall GPU Memory Requirement for all {module_name}: {val} MiB")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/measure_runtime.py
@@ -0,0 +1,98 @@
+from argparse import ArgumentParser
+from time import perf_counter
+
+import mmengine
+import torch
+from torch.utils.data import DataLoader
+from transformers import BatchEncoding
+
+from dissect.datasets import build_dataset
+from dissect.models import build_model_and_tokenizer
+
+
+class RuntimeHook:
+    def __init__(self) -> None:
+        self.in_time = 0.0
+        self.out_time = 0.0
+        self.starter = torch.cuda.Event(enable_timing=True)
+        self.ender = torch.cuda.Event(enable_timing=True)
+        self.elapsed_time = 0.0
+
+    def pre_hook(self, module, input):
+        self.in_time = perf_counter()
+        self.starter.record()
+
+    def hook(self, module, input, output):
+        self.out_time = perf_counter()
+        self.ender.record()
+        self.ender.synchronize()
+        self.elapsed_time = self.starter.elapsed_time(self.ender)
+
+    def __call__(self, pre: bool):
+        if pre:
+            return self.pre_hook
+        else:
+            return self.hook
+
+    def get_inference_time(self):
+        # return self.out_time - self.in_time
+        return self.elapsed_time
+
+
+def register_runtime_hook(module):
+    hook = RuntimeHook()
+    module.register_forward_pre_hook(hook(pre=True))
+    module.register_forward_hook(hook(pre=False))
+    return hook
+
+
+def parse_args():
+    parser = ArgumentParser("Test pruned models")
+    parser.add_argument("--config", default="./configs/prune_vicuna.yaml", help="Path to config file.")
+    parser.add_argument("--gpu-id", type=int, default=0, help="GPU ID.")
+    parser.add_argument(
+        "--cfg-options",
+        "-o",
+        nargs="+",
+        action=mmengine.DictAction,
+        help="Override the config entries with format xxx=yyy or xxx.zzz.qqq=yyy .",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    cfg = mmengine.Config.fromfile(args.config)
+    device = torch.device(f"cuda:{args.gpu_id}")
+    model, tokenizer = build_model_and_tokenizer(cfg.model, device=device)
+    model.to(device).eval()
+    dataset = build_dataset(cfg.test_dataset, tokenizer=tokenizer)
+    data_loader = DataLoader(dataset, **cfg.data_loader)
+
+    # register runtime hooks for interested modules
+    interest_modules = ["self_attn", "mlp", "overall"]
+    hooks_dict = dict()
+    for module in interest_modules:
+        hooks_dict.update({module: []})
+    hooks_dict.update({"overall": [register_runtime_hook(model)]})
+    for name, module in model.named_modules():
+        module_name = name.split(".")[-1]
+        if module_name in interest_modules:
+            hooks_dict[module_name].append(register_runtime_hook(module))
+
+    with torch.no_grad():
+        # run inference on one single batch
+        data = BatchEncoding(next(iter(data_loader))).to(device)
+        _ = model(**data)
+
+    # get inference time for interested modules
+    for module_name, hooks in hooks_dict.items():
+        inference_time = 0.0
+        for hook in hooks:
+            inference_time += hook.get_inference_time()
+        print(f"Total inference time for all {module_name}: {inference_time:.4f}")
+
+
+if __name__ == "__main__":
+    main()
