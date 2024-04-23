4d9da0587d2ba14f4a365a3640a8bfe08b7d0059
Yawei Li <yawei.li.muc@gmail.com>
2024-04-23 16:47:51 +0800

    amend commit for MacsCounter (WIP)

--- dissect/evaluators/__init__.py
+++ dissect/evaluators/__init__.py
@@ -2,6 +2,7 @@ from .accuracy import Accuracy
 from .builder import EVALUATORS
 from .inference_time import InferenceTime
 from .lm_eval_harness import LMEvalHarness
+from .macs_counter import MacsCounter
 from .perplexity import Perplexity
 
-__all__ = ["EVALUATORS", "Accuracy", "Perplexity", "InferenceTime", "LMEvalHarness"]
+__all__ = ["EVALUATORS", "Accuracy", "Perplexity", "InferenceTime", "LMEvalHarness", "MacsCounter"]
