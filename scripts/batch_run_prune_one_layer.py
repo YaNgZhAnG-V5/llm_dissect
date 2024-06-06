2e3799b16588a0efd9dc361d13261c05bd7030a2
Yang Zhang <zhangyang9523@gmail.com>
2024-06-06 23:46:50 +0800

    update the batch pruning script

--- scripts/batch_run_prune_one_layer.py
+++ scripts/batch_run_prune_one_layer.py
@@ -7,6 +7,7 @@ def main():
     parser.add_argument("--gpu-id", nargs="+", type=str)
     parser.add_argument("--model", type=str)
     parser.add_argument("--task", type=str)
+    parser.add_argument("--shapley", action="store_true")
     args = parser.parse_args()
     if "8b" in args.model:
         dim = 4096
@@ -17,19 +18,23 @@ def main():
     else:
         raise ValueError(f"Unsupported model {args.model}")
     gpu_id_str = ",".join(args.gpu_id)
-    if args.task == "mmlu" or "mmlu_zeroshot" or "perplexity" in args.task:
+    if args.shapley:
+        script_name = "layer_shapley"
+    else:
+        script_name = "prune_one_layer"
+    if (args.task == "mmlu") or (args.task == "mmlu_zeroshot") or ("perplexity" in args.task):
         os.system(
-            f"CUDA_VISIBLE_DEVICES={gpu_id_str} python scripts/prune_one_layer.py"
+            f"CUDA_VISIBLE_DEVICES={gpu_id_str} python scripts/{script_name}.py"
             f" --config ./configs/prune_one_layer_diff_tasks/{args.model}_{args.task}.yaml"
             f" --gpu-id 0 -d {dim}"
-            f" --workdir workdirs/prune_one_layer/{args.model}/{args.task}"
+            f" --workdir workdirs/{script_name}/{args.model}/{args.task}"
         )
     else:
         os.system(
-            f"CUDA_VISIBLE_DEVICES={gpu_id_str} python scripts/prune_one_layer.py"
+            f"CUDA_VISIBLE_DEVICES={gpu_id_str} python scripts/{script_name}.py"
             f" --config ./configs/prune_one_layer_diff_tasks/{args.model}_lm_eval.yaml"
             f" --gpu-id 0 -d {dim}"
-            f" --workdir workdirs/prune_one_layer/{args.model}/{args.task}"
+            f" --workdir workdirs/{script_name}/{args.model}/{args.task}"
             f" --cfg-options test_cfg.evaluator.lm_eval_cfg.tasks=['{args.task}']"
         )
 
