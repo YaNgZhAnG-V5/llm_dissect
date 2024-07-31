912334de96dc7ae7440e7083e870897ab04575fd
Xinpeng Wang <xinpeng.wang@tum.de>
2024-07-31 18:32:06 +0200

    llama guard evalautor

---
+++ dissect/evaluators/llama_guard_3.py
@@ -0,0 +1,109 @@
+import logging
+
+import torch
+import torch.nn as nn
+from alive_progress import alive_it
+from torch.utils.data import DataLoader
+
+from ..utils import Device
+from .builder import EVALUATORS
+from ..external_evaluators import AbsoluteHarmfulnessPredictor
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer
+)
+
+@EVALUATORS.register_module()
+class LlamaGuard3Evaluator(nn.Module):
+    
+    def __init__(self, tokenizer) -> None:
+        super().__init__()
+        setup = "redteam"  # or "redteam-osst"
+        model_id = "meta-llama/Llama-Guard-3-8B"
+        self.tokenizer = tokenizer
+        
+        
+        # self.llama_gard = AbsoluteHarmfulnessPredictor(setup)
+        self.llama_guard = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16, device_map='cuda')
+        self.tokenizer_guard = AutoTokenizer.from_pretrained(model_id)
+        self.tokenizer_guard.padding_side = "left"
+        self.tokenizer_guard.pad_token = self.tokenizer_guard.eos_token
+        
+    @torch.no_grad()
+    def moderate_batch(self, batch):
+        formatted_inputs = [
+            self.tokenizer_guard.apply_chat_template(conv, tokenize=False, add_generation_prompt=True)
+            for conv in batch
+        ]
+
+        model_inputs = self.tokenizer(
+            formatted_inputs,
+            return_tensors="pt",
+            padding=True,
+            # truncation=True
+        ).to("cuda")
+
+        terminators = [
+            self.tokenizer_guard.eos_token_id,
+            self.tokenizer_guard.convert_tokens_to_ids("<|eot_id|>")
+        ]
+
+        generated_ids = self.llama_guard.generate(**model_inputs, eos_token_id=terminators, max_new_tokens=128)
+
+        return self.tokenizer.batch_decode(generated_ids[:, model_inputs['input_ids'].shape[1]:], skip_special_tokens=True)
+
+    @torch.no_grad()
+    def evaluate(
+        self,
+        model: nn.Module,
+        sparsity: float,
+        data_loader: DataLoader,
+        device: Device,
+        logger: logging.Logger,
+        method_name: str,
+        verbose: bool = True,
+    ) -> float:
+
+        harmfulness_scores = []
+        for batch in alive_it(data_loader, total=len(data_loader), enrich_print=False, disable=not verbose):
+            
+            
+            #TODO: apply chat template here
+            input_prompt = batch["text"]
+            
+            conversation = [
+                  [{"role": "user", "content": i }] for i in input_prompt
+            ]
+            formatted_inputs = [
+                self.tokenizer.apply_chat_template(conv, tokenize=False, add_generation_prompt=True)
+                for conv in conversation
+            ]
+            model_inputs = self.tokenizer(
+                formatted_inputs,
+                return_tensors="pt",
+                padding=True,
+                # truncation=True
+            ).to("cuda")
+            #TODO: this terminators is llama3 specific thing, need to change it to be more generic
+            terminators = [
+                self.tokenizer.eos_token_id,
+                self.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+            ]
+            generated_ids = model.generate(**model_inputs, eos_token_id=terminators, max_new_tokens=128)
+            responses = self.tokenizer.batch_decode(generated_ids[:, model_inputs['input_ids'].shape[1]:], skip_special_tokens=True)
+            user_texts = [conv[0]["content"] for conv in conversation]
+            merged_batch = []
+            for i in range(len(conversation)):
+                merged_entry = conversation[i] + [{'role': 'assistant', 'content': responses[i]}]
+                merged_batch.append(merged_entry)
+            
+            
+            logger.info(f"Sparsity: {sparsity}\n[Input prompt]: {input_prompt}\n[Generated text]: {responses}")
+            
+            guard_pred = self.moderate_batch(merged_batch)
+
+            # logger. info(f"Harumfulness score: {harumfulness_score}")
+        # average the list 
+        avg_harmfulness_score = sum(harmfulness_scores) / len(harmfulness_scores)       
+        
+        return avg_harmfulness_score
