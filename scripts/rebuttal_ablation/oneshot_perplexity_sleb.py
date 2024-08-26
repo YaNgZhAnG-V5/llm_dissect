2787a3ae84a4ef322980b2b8ae0c6e9c7bdcab4c
Yang Zhang <zhangyang9523@gmail.com>
2024-08-26 07:49:16 +0000

    bugfix

--- scripts/rebuttal_ablation/oneshot_perplexity_sleb.py
+++ scripts/rebuttal_ablation/oneshot_perplexity_sleb.py
@@ -21,18 +21,21 @@ def main():
     perplexity_str = "_perp" if args.perplexity else ""
     exclude_str = " -x 0.4" if args.exclude else ""
     one_shot_str = " -p loss_one_shot" if args.one_shot else ""
-
+    if args.model == "llama3_70b" or args.model == "llama2_70b":
+        dimension = 8192
+    else:
+        dimension = 4096
     # perform pruning
     if args.prune:
         if args.sleb:
             # sleb use perplexity, perform one-shot pruning on decoders, without excluding the first 40%
             os.system(
-                f"CUDA_VISIBLE_DEVICES={device_str} python scripts/prune_layer.py "
-                f"--config configs/{args.model}_perp.yaml -p loss_one_shot -lv decoder -w {args.workdir}"
+                f"CUDA_VISIBLE_DEVICES={device_str} python scripts/prune_layer.py -d {dimension} "
+                f"--config configs/{args.model}_perp.yaml -lv decoder -w {args.workdir}"
             )
         else:
             os.system(
-                f"CUDA_VISIBLE_DEVICES={device_str} python scripts/prune_layer.py "
+                f"CUDA_VISIBLE_DEVICES={device_str} python scripts/prune_layer.py -d {dimension} "
                 f"--config configs/{args.model}{perplexity_str}.yaml{exclude_str}{one_shot_str} -w {args.workdir}"
             )
 
